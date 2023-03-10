919ff0d3ec3227dbc53ced31f8279fe472e53b09
qwo330 <qwd330@google.co.kr>
2023-03-11 04:30:24 +0900

    플레이어 코드 정리

--- ToyRPG1/Assets/Scripts/PlayerControl.cs
+++ ToyRPG1/Assets/Scripts/PlayerControl.cs
@@ -13,16 +13,15 @@ public enum EPlayerState
 
 public class PlayerControl : SingletonMono<PlayerControl>
 {
-    public float _Speed = 6.0f;
-    public float _JumpSpeed = 8.0f;
-    public float _Gravity = 10.0f;
-    public float _RotSpeed = 5.0f;
-    public float _AttackCoolTime = 2.0f; // 공격 애니메이션 시간
-    public float _AttackInputTime = 1.0f; // 2타 입력 대기 시간
-
-    public float testStartAnim = 0.6f;
-    public float testEndAnim = 1f;
+    public float Speed = 6.0f;
+    public float RotSpeed = 5.0f;
     
+    public float JumpSpeed = 8.0f;
+    public float Gravity = 10.0f;
+    
+    public float AttackCoolTime = 2.0f; // 공격 애니메이션 시간
+    public float AttackInputTime = 1.0f; // 2타 입력 대기 시간
+
     /*
      * 1타 공격 애니메이션 25프레임 x 1.0배속
      * 0.01667 * 25 = 0.41675 -> 0.4
@@ -31,22 +30,22 @@ public class PlayerControl : SingletonMono<PlayerControl>
      * 0.01667 * 25 / 0.6 = 0.69458 -> 0.7
      * */
 
-    [HideInInspector]
-    public Transform _Trans;
-    public GameObject _FollowTarget;
+    [HideInInspector] public Transform Trans;
+    public GameObject FollowTarget;
 
-    Animator _playerAnim;
-    CharacterController _controller;
+    Animator playerAnim;
+    CharacterController controller;
     Camera playerCamera;
 
-    Vector3 _motion;
-    
     [SerializeField]
-    EPlayerState _currentState;
+    EPlayerState currentState;
 
     bool isAttack = false;
     float combo = 0;
     int currentHash = 0, prevHash = 0;
+    
+    EPlayerState playState = EPlayerState.Idle;
+    Vector3 motion = Vector3.zero;
     
     static readonly int HASH_JUMP = Animator.StringToHash("Jump");
     static readonly int HASH_RUN = Animator.StringToHash("Run");
@@ -54,18 +53,15 @@ public class PlayerControl : SingletonMono<PlayerControl>
 
     void Start()
     {
-        _Trans = GetComponent<Transform>();
-        _controller = GetComponent<CharacterController>();
-        _playerAnim = GetComponentInChildren<Animator>();
+        Trans = GetComponent<Transform>();
+        controller = GetComponent<CharacterController>();
+        playerAnim = GetComponentInChildren<Animator>();
 
         playerCamera = Camera.main;
-
-        _motion = Vector3.zero;
-        _currentState = EPlayerState.Idle;
+        
+        motion = Vector3.zero;
+        currentState = EPlayerState.Idle;
     }
-
-    EPlayerState playState = EPlayerState.Idle;
-    float animValue = 0;
 
     void Update()
     {
@@ -79,8 +75,6 @@ public class PlayerControl : SingletonMono<PlayerControl>
         // update의 마지막에 playState, animValue를 매개변수로 SetPlayerAnimation를 호출해서 이번 업데이트의 동작을 결정한다?
 
         Attack();
-
-        //Attack();
         Move();
     }
 
@@ -94,7 +88,7 @@ public class PlayerControl : SingletonMono<PlayerControl>
         EPlayerState animState = EPlayerState.Idle;
         float animatorValue = 0;
 
-        if (_controller.isGrounded == false)
+        if (controller.isGrounded == false)
         {
             animState = EPlayerState.Idle;
         }
@@ -111,52 +105,44 @@ public class PlayerControl : SingletonMono<PlayerControl>
                 forward.y = 0;
                 forward = forward.normalized;
 
-                _motion = forward * _Speed;
+                motion = forward * Speed;
 
                 animState = EPlayerState.Run;
-                animatorValue = _motion.sqrMagnitude;
+                animatorValue = motion.sqrMagnitude;
 
                 // rotate
                 Quaternion rotation = Quaternion.LookRotation(forward);
-                _Trans.rotation = rotation;
+                Trans.rotation = rotation;
             }
             else // 조작 없을 때
             {
-                _motion = Vector3.zero;
+                motion = Vector3.zero;
                 animState = EPlayerState.Idle;
             }
 
             if (Input.GetButton("Jump"))
             {
-                _motion.y = _JumpSpeed;
+                motion.y = JumpSpeed;
                 animState = EPlayerState.Jump;
             }
         }
 
         // always
-        _motion.y -= _Gravity * Time.deltaTime;
-        _controller.Move(_motion * Time.deltaTime);
+        motion.y -= Gravity * Time.deltaTime;
+        controller.Move(motion * Time.deltaTime);
         SetPlayerAnimation(animState, animatorValue);
-
-        Debug.LogError($"animState : {animState}");
     }
-
-    int _numClicks = 0;
-    int curCombo = 0;
-    int attackIndex = 0;
-    float _lastClickTime = 0;
-    float _comboDelay = 1.2f;
-
+    
     void Attack()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (_currentState == EPlayerState.Run)
+            if (currentState == EPlayerState.Run)
             {
-                SetStatMoveToIdle();
+                ChangeStateToIdle();
             }
             
-            if (_playerAnim.GetFloat("Combo") == 0)
+            if (playerAnim.GetFloat(HASH_COMBO) == 0)
             {
                 SetPlayerAnimation(EPlayerState.Attack, 1);
             }
@@ -184,81 +170,7 @@ public class PlayerControl : SingletonMono<PlayerControl>
             //}
         }
     }
-
-    void Attack_old()
-    {
-        /*
-         * 콤보어택 애니메이션 참고 링크
-         * https://answers.unity.com/questions/1192912/hot-to-make-combo-attack-1-button-do-3-things.html
-         * https://samirgeorgy.wordpress.com/2021/07/22/lets-create-a-simple-melee-combo-system/
-         */
-
-        bool attackFlag = false;
-
-        if (Input.GetMouseButton(0))
-        {
-            // 최초 공격
-            if (combo == 0)
-            {
-                Debug.LogError("attack !!");
-
-                attackFlag = true;
-
-                combo = 1;
-                isAttack = true;
-
-                SetPlayerAnimation(EPlayerState.Attack, combo);
-
-                var stateInfo = _playerAnim.GetCurrentAnimatorStateInfo(0);
-                currentHash = stateInfo.shortNameHash;
-                //Debug.LogError(phase + " time? " + stateInfo.length);
-            }
-            else // 연속으로 공격 입력이 올 때
-            {
-                // 다음 공격 입력 타이밍이고, 다음 모션이 있을 때
-                if (IsInputTime() && IsSameAnimation() == false)
-                {
-
-                    attackFlag = true;
-
-                    prevHash = currentHash;
-
-                    combo++;
-                    SetPlayerAnimation(EPlayerState.Attack, combo);
-
-                    var stateInfo = _playerAnim.GetCurrentAnimatorStateInfo(0);
-                    currentHash = stateInfo.shortNameHash;
-                    
-                    Debug.LogError(combo + " time? " + stateInfo.length);
-                }
-            }
-        }
-
-        if (isAttack)
-        {
-            var animStateInfo = _playerAnim.GetCurrentAnimatorStateInfo(1);
-            Debug.LogError($"=== anim hash : {animStateInfo.shortNameHash}, time : {animStateInfo.normalizedTime}");
-            //ShowCurrentAnimName();
-        }
-
-        // 공격이 종료될 때까지 다음 입력이 없을 때
-        // 애니메이션이 바로 끝나버려서(왠지 모르겠음)
-        // flag가 true이면 현재 루프에선 호출 하지 않는다.
-        if (isAttack && IsFinishTime() && attackFlag == false)
-        {
-            currentHash = 0;
-            prevHash = 0;
-
-            isAttack = false;
-            combo = 0;
-
-            SetPlayerAnimation(EPlayerState.Attack, 0);
-            SetPlayerAnimation(EPlayerState.Idle);
-
-            Debug.LogError("FINISH !!");
-        }
-    }
-
+    
     void FinishAttack()
     {
         currentHash = 0;
@@ -271,42 +183,14 @@ public class PlayerControl : SingletonMono<PlayerControl>
 
         Debug.LogError("FINISH !!");
     }
-
-    bool IsInputTime()
-    {
-        var animStateInfo = _playerAnim.GetCurrentAnimatorStateInfo(0);
-
-        if (testStartAnim <= animStateInfo.normalizedTime && animStateInfo.normalizedTime < testEndAnim)
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-    bool IsFinishTime()
-    {
-        var animStateInfo = _playerAnim.GetCurrentAnimatorStateInfo(0);
-        if (animStateInfo.normalizedTime >= 1.0f)
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-    bool IsSameAnimation()
-    {
-        return prevHash == currentHash;
-    }
-
+    
     void ChangeState(EPlayerState nextState)
     {
         // todo : 이걸로 상태 변경하고 프로세스를 통해 SetPlayerAnimation 호출하는게 좋을 듯
-        _currentState = nextState;
+        currentState = nextState;
     }
 
-    void SetStatMoveToIdle()
+    void ChangeStateToIdle()
     {
         // todo : 이동 중 공격을 하면 애니메이터가 꼬인다. (애니메이터는 run, 코드는 attack)
         // ChangeState(EPlayerState.Idle);
@@ -315,28 +199,28 @@ public class PlayerControl : SingletonMono<PlayerControl>
 
     public void SetPlayerAnimation(EPlayerState state, float _animatorValue = 0)
     {
-        if (_currentState != state)
+        if (currentState != state)
         {
-            var saveState = _currentState;
-            Debug.LogError($"Change State : {saveState} -> {state}");
+            EPlayerState saveState = currentState;
+            // Debug.LogError($"Change State : {saveState} -> {state}");
         }
 
         // 뛰면서 바로 공격하면 멈춤, state가 attack이 되어 이동은 먹통, 애니메이션은 전환이 안됨
         // 코드의 state는 바로 바뀌고 animation은 바뀌지 않아 버그 발생함
-        _currentState = state;
+        currentState = state;
         
         switch (state)
         {
             case EPlayerState.Run:
-                _playerAnim.SetFloat(HASH_RUN, _animatorValue);
+                playerAnim.SetFloat(HASH_RUN, _animatorValue);
                 break;
             case EPlayerState.Jump:
-                _playerAnim.SetTrigger(HASH_JUMP);
+                playerAnim.SetTrigger(HASH_JUMP);
                 break;
             case EPlayerState.Attack:
                 //_playerAnim.SetFloat("Combo", _animatorValue);
                 //_playerAnim.SetTrigger("Attack");
-                _playerAnim.SetFloat(HASH_COMBO, _animatorValue);
+                playerAnim.SetFloat(HASH_COMBO, _animatorValue);
 
                 if (_animatorValue == 0)
                 {
@@ -349,15 +233,15 @@ public class PlayerControl : SingletonMono<PlayerControl>
                 break;
             default:
             case EPlayerState.Idle:
-                _playerAnim.SetFloat(HASH_RUN, 0);
+                playerAnim.SetFloat(HASH_RUN, 0);
                 break;
         }
     }
 
     bool IsActionAble()
     {
-        return _currentState != EPlayerState.Dead
-            && _currentState != EPlayerState.Hit
-            && _currentState != EPlayerState.Attack;
+        return currentState != EPlayerState.Dead
+            && currentState != EPlayerState.Hit
+            && currentState != EPlayerState.Attack;
     }
 }
