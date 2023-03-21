bab7d7dc3a66a0373b958160052e45460692ed98
qwo330 <qwd330@google.co.kr>
2023-03-21 17:35:01 +0900

    투사체 코드 수정 및 프리팹 수정

--- ToyRPG1/Assets/Scripts/Projectile.cs
+++ ToyRPG1/Assets/Scripts/Projectile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,14 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
+    [SerializeField] float sqrEffectiveRange = 25f;
     
+    Vector3 startPoint;
     Vector3 direction;
-    public Vector3 Direction
-    {
-        set => direction = value;
-    }
-    
     int power;
-    public int Power
-    {
-        set => power = value;
-    }
+    bool isPlaying;
+    
+    bool IsInvaildRange => (transform.position - startPoint).sqrMagnitude > sqrEffectiveRange;
     
     MyObjectPool<Projectile> pool;
     public MyObjectPool<Projectile> Pool
@@ -26,26 +23,62 @@ public class Projectile : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (direction == Vector3.zero)
+        if (isPlaying == false)
         {
-            MyDebug.LogError("Direction is not set");
+            return;
         }
-        
+
+        if (IsInvaildRange)
+        {
+            OnReturnToPool();
+        }
+
         transform.position += direction * moveSpeed * Time.deltaTime;
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Map"))
+        {
+            OnReturnToPool();
+        }
+        else if (other.CompareTag("Monster"))
+        {
+            Actor target = other.GetComponent<Actor>();
+            Hit(target);
+        }
+    }
+
+    public void Play(Vector3 direction, int power)
+    {
+        this.startPoint = transform.position;
+        this.direction = direction;
+        this.power = power;
+        
+        isPlaying = true;
+    }
+    
     public void Hit(Actor target)
     {
+        if (target == null)
+        {
+            MyDebug.LogError("Target is Null");
+            return;
+        }
+        
         if (power == 0)
         {
             MyDebug.LogError("Power is not set");
         }
         
         target.TakeDamage(power);
+        OnReturnToPool();
     }
     
     void OnReturnToPool()
     {
+        isPlaying = false;
+        
         pool.Release(this);
     }
 }
